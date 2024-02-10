2b883ccd94714f3717dbcc12898046bc1e4b962b
Christopher Wuni <christdam55@gmail.com>
2024-02-10 14:03:08 +0100

    cleanup on main and app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config({});
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.setGlobalPrefix('api/v1/');
-  await app.listen(3000);
+  app.setGlobalPrefix('/api/v1/', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
