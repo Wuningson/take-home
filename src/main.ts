8d9e8237f2cc2552d14269c110cfdf0400787732
Christopher Wuni <christdam55@gmail.com>
2024-02-10 14:26:53 +0100

    added morgan

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   dotenv.config({});
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('combined'));
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('/api/v1/', {
